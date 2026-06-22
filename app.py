d1fcbec4528309f8ec788f3cad387caf313caaf2
vikhebb-glitch <vikhebb@pravaraengg.org.in>
2026-06-21 20:58:50 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,19 +3,15 @@ import numpy as np
 from PIL import Image
 import tflite_runtime.interpreter as tflite
 
-# Class Names
-
 class_names = [
-'downy_mildew',
-'healthy',
-'leaf_blight',
-'purple_blotch'
+    'downy_mildew',
+    'healthy',
+    'leaf_blight',
+    'purple_blotch'
 ]
 
-# Load TFLite Model
-
 interpreter = tflite.Interpreter(
-model_path="onion_leaf_cnn_model_quant.tflite"
+    model_path="onion_leaf_cnn_model_quant.tflite"
 )
 
 interpreter.allocate_tensors()
@@ -23,73 +19,51 @@ interpreter.allocate_tensors()
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 
-# Title
-
 st.title("Onion Leaf Disease Detection")
 
 st.markdown("""
-
 ### Research Scholar
-
 **Bhausaheb Bhaskar Vikhe**
 
 ### Under the Guidance of
-
 **Dr. Purushottam Patil**
 
 ### Sandip University Campus, Nashik
-
 """)
 
-st.write("Upload Onion Leaf Image")
-
-# Upload Image
-
 uploaded_file = st.file_uploader(
-"Choose onion leaf image",
-type=["jpg", "jpeg", "png"]
+    "Choose onion leaf image",
+    type=["jpg", "jpeg", "png"]
 )
 
 if uploaded_file is not None:
 
-```
-img = Image.open(uploaded_file).convert("RGB")
+    img = Image.open(uploaded_file).convert("RGB")
+    st.image(img, caption="Uploaded Image")
 
-st.image(img, caption="Uploaded Image")
+    img = img.resize((224, 224))
 
-img = img.resize((224, 224))
+    img_array = np.array(img, dtype=np.float32)
+    img_array = img_array / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
 
-img_array = np.array(img, dtype=np.float32)
+    interpreter.set_tensor(
+        input_details[0]['index'],
+        img_array
+    )
 
-img_array = img_array / 255.0
+    interpreter.invoke()
 
-img_array = np.expand_dims(img_array, axis=0)
+    prediction = interpreter.get_tensor(
+        output_details[0]['index']
+    )
 
-# Prediction
-interpreter.set_tensor(
-    input_details[0]['index'],
-    img_array
-)
+    predicted_class = np.argmax(prediction)
 
-interpreter.invoke()
+    confidence = float(np.max(prediction)) * 100
 
-prediction = interpreter.get_tensor(
-    output_details[0]['index']
-)
+    result = class_names[predicted_class]
 
-predicted_class = np.argmax(prediction)
-
-confidence = float(np.max(prediction)) * 100
-
-result = class_names[predicted_class]
-
-st.subheader("Prediction Result")
-
-st.success(
-    f"Detected Disease: {result}"
-)
-
-st.info(
-    f"Confidence: {confidence:.2f}%"
-)
-```
+    st.subheader("Prediction Result")
+    st.success(f"Detected Disease: {result}")
+    st.info(f"Confidence: {confidence:.2f}%")
